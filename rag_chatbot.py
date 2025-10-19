7deda99c55d01c2a967672cdb476f9c250057e86
Akshay Sasi <akshaysasi12.knr@gmail.com>
2025-10-20 00:42:13 +0530

    Fix import path for create_retrieval_chain

--- rag_chatbot.py
+++ rag_chatbot.py
@@ -5,7 +5,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import ChatGoogleGenerativeAI 
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
+from langchain_core.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.vectorstores import FAISS
 
